11f9977f5036fa706620411db9cb5d9cfe0626d3
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-24 22:22:34 -0300

    Just renaming to app

---
+++ backend/app.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from groq import Groq
+from typing import List, Dict
+from dotenv import load_dotenv
+import os
+
+from backend.schema import UserInput, Conversation
+from backend.model import chat_with_groq
+
+
+
+app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+conversations: Dict[str, Conversation] = {}
+
+# Salva, cria e retorna as conversas tidas baseadas no ID delas
+def get_or_create_conversation(conversation_id: str) -> Conversation:
+  if conversation_id not in conversations:
+    conversations[conversation_id] = Conversation()
+  return conversations[conversation_id]
+
+@app.post("/chat")
+async def chat(user_input: UserInput):
+  conversation = get_or_create_conversation(user_input.conversation_id)
+
+  if not conversation.active:
+    raise HTTPException(status_code=400, detail="Conversation is inactive")
+  
+  try:
+    conversation.messages.append({
+      "role": user_input.role,
+      "content": user_input.message
+    })
+
+    response = chat_with_groq(conversation)
+
+    conversation.messages.append({
+      "role": "system",
+      "content": response
+    })
+
+    return {
+      "response" : response,
+      "conversation_id": user_input.conversation_id,
+    }
+  except Exception as e:
+    raise HTTPException(status_code=500, detail="Error while processing the request") from e
+
+
+
