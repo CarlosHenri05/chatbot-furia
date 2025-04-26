73d95982d96feee3f082bb76b8c702aee1173b85
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-26 20:27:11 -0300

    Adding files for front-end

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'furia-purple': '#6A0DAD ',
+        'furia-black': '#000000',
+      },
+    },
+  },
+  plugins: [],
+}
